924a96878195c5678e2242db34998257937a0d6c
fabioarduin <fabioarduin@gmail.com>
2022-05-03 13:00:00 -0300

    Create alteracodigo.js
    cria arquivo com sugestões de melhoria do código do projeto

---
+++ alteracodigo.js
@@ -0,0 +1,32 @@
+function juntar(){
+    sum
+    div
+    mult
+    sub
+}
+
+function comandos(){
+    let num1 = 10;
+    let num2 = 5;
+    let sum = num1 + num2;
+    console.log(sum)
+
+    let num3= 10;
+    let num4 = 5;
+    let sub = num3 - num4;
+    console.log(sub)
+
+    let num5 = 5;
+    let num6 = 10;
+    let mult = num5 * num6;
+    console.log(mult)
+
+    let num7 = 10;
+    let num8 = 5;
+    let div = num7 / num8;
+    console.log(div)
+}
+
+function console(){;
+    console.log("apresenta:" (juntar), (comandos))
+}
