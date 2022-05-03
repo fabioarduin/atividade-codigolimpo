4f87310d16342eff3cb3f7c033770121c8b96d09
fabioarduin <fabioarduin@gmail.com>
2022-05-03 12:42:18 -0300

    Create codigo limpo.js
    cria arquivo codigo limpo.js

---
+++ codigo limpo.js
@@ -0,0 +1,32 @@
+function sum(){
+    let num1 = 10;
+    let num2 = 5;
+    let sub = num1 + num2;
+    console.log(sub)
+}
+
+
+function sub(){
+    let num1 = 10;
+    let num2 = 5;
+    let sub = num1 - num2;
+    console.log(sub)
+
+}
+
+console.log(sub() + sum())
+
+function mult(){
+    let num1 = 5;
+    let num2 = 10;
+    let mult = num1 * num2;
+    console.log(mult)
+}
+
+function div(){
+    //TODO: Implementar divisão aqui
+    let num1 = 10;
+    let num2 = 5;
+    let sub = num1 / num2;
+    console.log(div)
+}
