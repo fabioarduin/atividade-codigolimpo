5c8f329357d056d002c6e93b6e4d67db9ac21259
Gregory Moraes <gregorymoraes1@hotmail.comgit config --global user.email gregorymoraes1@hotmail.comgit config --global user.email gregorymoraes1@hotmail.com>
2022-05-10 11:26:39 -0300

    :aparkles:Update

---
+++ codigoLimpo.js
@@ -0,0 +1,27 @@
+let primeiroNumero = 10;
+let segundoNumero = 5;
+
+function sum(primeiroNumero, segundoNumero){
+  return primeiroNumero + segundoNumero
+}
+
+
+function sub(primeiroNumero, segundoNumero){
+    return  primeiroNumero - segundoNumero
+}
+
+function mult(primeiroNumero, segundoNumero){
+   return primeiroNumero * segundoNumero
+  }
+
+function div(primeiroNumero, segundoNumero){
+    return primeiroNumero / segundoNumero  
+}
+
+
+function calc() {
+    console.log(sum)
+    console.log(sub)
+    console.log(mult)
+    console.log(div)
+}
